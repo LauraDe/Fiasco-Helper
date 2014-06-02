0f383b1e11ea9780cd7a294a90cd2b88ed38832b
Laura D <Kittywizard@Lauras-MacBook-Pro.local>
2014-06-02 15:37:31 -0700

    Initial Commit

---
+++ Fiasco Helper/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Fiasco Helper
+//
+//  Created by Laura D. on 6/2/14.
+//  Copyright (c) 2014 Laura Delaney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
